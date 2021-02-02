78e412806ba244b0e4c6ac51c790d28a138fbf69
Daniel Denley <daniel-denley@live.co.uk>
2021-02-02 18:10:49 +0000

    Added login backend, Added Lab Manager user authentication - Does not render body if invalid access, instead error rendered

---
+++ BackEnd/UserPer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseworkAC31007Agile.BackEnd
+{
+    public class UserPer
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Access { get; set; }
+        
+    }
+}
