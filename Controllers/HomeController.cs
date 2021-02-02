9825931c9c77b41d28cd26c29f24faf1b2859266
Alek Alexiev <aalexiev@dundee.ac.uk>
2021-02-02 10:38:06 +0000

    Share Questionnaire

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Bind_DropDownList_Core.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace CourseworkAC31007Agile.Controllers
+{
+    //Adapted from https://www.aspsnippets.com/Articles/Populating-DropDownList-inside-Razor-Pages-in-ASPNet-Core-MVC.aspx
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<DropdownModel> dropdownTable = PopulateDropdown();
+            return View(new SelectList(dropdownTable, "Id", "title"));
+        }
+
+        [HttpPost]
+        public IActionResult Index(string Id, string title)
+        {
+            ViewBag.Message = "title: " + title;
+            ViewBag.Message += "\\nId: " + Id;
+            return View();
+        }
+
+        private static List<DropdownModel> PopulateDropdown()
+        {
+            string constr = @"Data Source=agile-coursework-server.database.windows.net;Initial Catalog=StorageForTheB;integrated security=true";
+            List<DropdownModel> dropdownTable = new List<DropdownModel>();
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                string query = "SELECT title, Id FROM Questionnaires";
+                using (SqlCommand cmd = new SqlCommand(query))
+                {
+                    cmd.Connection = con;
+                    con.Open();
+                    using (SqlDataReader sdr = cmd.ExecuteReader())
+                    {
+                        while (sdr.Read())
+                        {
+                            dropdownTable.Add(new DropdownModel
+                            {
+                                title = sdr["title"].ToString(),
+                                Id = Convert.ToInt32(sdr["Id"])
+                            });
+                        }
+                    }
+                    con.Close();
+                }
+            }
+
+            return dropdownTable;
+        }
+    }
+}
---
+++ Model/DropdownModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Bind_DropDownList_Core.Model
+{
+    //Adapted from https://www.aspsnippets.com/Articles/Populating-DropDownList-inside-Razor-Pages-in-ASPNet-Core-MVC.aspx
+    public class DropdownModel
+    {
+        public int Id { get; set; }
+        public string title { get; set; }
+    }
+}
