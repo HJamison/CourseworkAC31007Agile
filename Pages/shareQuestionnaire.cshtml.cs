1faee1ac68f52ccfb0ec290e75cdbee5f750694c
Alek Alexiev <aalexiev@dundee.ac.uk>
2021-02-05 08:43:54 +0000

    Share Questionnaire now lists available questionnaires and shares them

--- Pages/shareQuestionnaire.cshtml.cs
+++ Pages/shareQuestionnaire.cshtml.cs
@@ -2,15 +2,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CourseworkAC31007Agile.BackEnd.DatabaseModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using Microsoft.AspNetCore.Session;
+using Microsoft.AspNetCore.Http;
 namespace CourseworkAC31007Agile.Pages
 {
     public class shareQuestionnaireModel : PageModel
     {
         public void OnGet()
         {
+            var questionnaireList = new List<Questionnaires>();
+            var questionnaireTitle = new List<string>();
+            var questionnaireID = new List<int>();
+            using (var db = new DatabaseContext())
+            {
+                questionnaireList = db.Questionnaires.ToList();
+                foreach (var user in questionnaireList)
+                {
+                    questionnaireTitle.Add(user.Title);
+                    questionnaireID.Add(user.Id);
+                }
+            }
         }
     }
 }
